7a8275553ac8ab3d144f955e2d49c0049269f169
Davi Inácio <aazz6850@gmail.com>
2019-06-22 21:33:07 -0300

    Page update

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#callGame').click(function(){
+        $('#callGame').css('display', 'none');
+        $('#gameFrame').css('display', 'block');
+        $('#gameFrame').attr('src',  'https://daviinacio.github.io/desenvolvendo-desenvolvedores/game/logica_programacao/');
+        //https://daviinacio.github.io/desenvolvendo-desenvolvedores/game/logica_programacao/
+    });
+})
